f3ccaeb49882d8354c991fc02267cd0812a442cc
Hboyss7 <150256806+Hboyss7@users.noreply.github.com>
2024-09-10 11:08:01 +0700

    Add files via upload

---
+++ 2212366_TestDelegate/2212366_TestDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2212366_TestDelegate
+{
+    internal class Program
+
+    {
+        public delegate int TinhToan(int a, int b);
+
+        public static int Cong ( int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Tru( int a, int b )
+        {
+            return a - b;
+        }
+        
+        static void Main(string[] args)
+        {
+            TinhToan tt = new TinhToan(Cong);
+
+            Console.WriteLine(tt(4,5));
+
+            Console.ReadKey();
+        }
+    }
+}
